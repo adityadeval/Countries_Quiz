afe03a18c3a0eb9e608619c9897abd32d6d5cb94
adityadeval <adityadeval27@gmail.com>
2024-04-09 03:25:54 -0400

    + App can move to fragment no. it was on before rotation. It can also remember choices selected by User on that fragment before rotation. However, it loads/creates new fragments/questions.
    Note : Issue is definately with the adapter. Hardcoded position = 6; might be the problem. Adapter should be made to realize that old fragments are saved and it needs to reload them and not create new ones.

--- app/src/main/java/ms/cs/countries_quiz/CountryQuizAdapter.java
+++ app/src/main/java/ms/cs/countries_quiz/CountryQuizAdapter.java
@@ -1,5 +1,7 @@
 package ms.cs.countries_quiz;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,6 +9,8 @@ import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class CountryQuizAdapter extends FragmentStateAdapter {
+
+    private static final String DEBUG_TAG = "CountryQuizAdapter";
     int position = 6;
     public CountryQuizAdapter(
             FragmentManager fragmentManager, Lifecycle lifecycle ) {
@@ -15,6 +19,7 @@ public class CountryQuizAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position){
+        Log.d(DEBUG_TAG, "Calling newInstance of CountryQuizFragment, position is : " + position);
         return CountryQuizFragment
                 .newInstance( position );
     }
--- app/src/main/java/ms/cs/countries_quiz/MainActivity.java
+++ app/src/main/java/ms/cs/countries_quiz/MainActivity.java
@@ -10,18 +10,42 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG_TAG = "MainActivity";
+    private static ViewPager2 pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d( DEBUG_TAG, "Inside onCreate()" );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager2 pager = findViewById( R.id.viewpager );
+        pager = findViewById( R.id.viewpager );
+
         CountryQuizAdapter avpAdapter = new
                 CountryQuizAdapter(
                 getSupportFragmentManager(), getLifecycle() );
         pager.setOrientation(
                 ViewPager2.ORIENTATION_HORIZONTAL );
         pager.setAdapter( avpAdapter );
+
+        // Below if checks if any previous app state has been saved (in case onCreate() is being called after an interrupt
+        // occured. In case there is a saved state, the key = currentFragmentNo is passed to fetch the fragment number
+        // the user was on, before the app was stopped.
+        // This fragment number is then used to open the exact fragment the user was on before the interrupt occured.
+        if (savedInstanceState != null) {
+            int currentPosition = savedInstanceState.getInt("currentFragmentNo", 0);
+            Log.d( DEBUG_TAG, "Inside onCreate() AGAIN. Fragment number being restored = " + currentPosition );
+            pager.setCurrentItem(currentPosition, false); // Set to the saved position
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d( DEBUG_TAG, "Inside onSaveInstanceState()" );
+        // Assuming 'viewPager' is your ViewPager2 instance
+        int currentPosition = pager.getCurrentItem();
+        outState.putInt("currentFragmentNo", currentPosition);
+        Log.d( DEBUG_TAG, "Saving fragment number in Bundle :" + currentPosition );
     }
 }
