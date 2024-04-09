605f53f7ec993b14df5b08dba9c709195f3d581f
ruchithavishwanath <ruchitha.rv@gmail.com>
2024-04-09 13:42:34 -0400

    replace fragment

--- app/src/main/java/ms/cs/countries_quiz/MainActivity.java
+++ app/src/main/java/ms/cs/countries_quiz/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG_TAG = "MainActivity";
@@ -23,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
         pager.setOrientation(
                 ViewPager2.ORIENTATION_HORIZONTAL );
         pager.setAdapter( avpAdapter );
+        pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 5) {
+                    Toast.makeText(MainActivity.this, "Last page", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        if( getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ) {
+            Log.d( DEBUG_TAG, "CountriesInfoActivity.onCreate(): in landscape mode; finishing" );
+            finish();
+            return;
+        }
     }
 }
